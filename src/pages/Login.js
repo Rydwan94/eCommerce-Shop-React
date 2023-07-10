141dab42486f039ecf33e47bfa1cb071007b8e53
rydwan94 <rydwan94@gmail.com>
2023-07-10 21:11:26 +0200

    loginPage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,38 @@
 import { Link } from "react-router-dom";
 
+import "../css/Login.css";
+
+import loginImg from "../img/loginImg.jpg";
+import { useEffect, useRef } from "react";
+
+
 const Login = () => {
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current.focus()
+  },[])
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="loginPage">
       <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" />
+        <section className="loginImage">
+          <img src={loginImg} alt="loginImage" />
+        </section>
+        <section className="loginForm">
+          <h3>Nice to see you again</h3>
+          <label htmlFor="username">Username:</label>
+          <input ref={inputRef} type="text" id="username" />
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
 
-        <button type="submit">Log in</button>
+          <button type="submit">Log in</button>
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </section>
       </form>
-
-      <p>
-        Don't have an account? <Link to="/register">Register</Link>
-      </p>
     </div>
   );
 };
